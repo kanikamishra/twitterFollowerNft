0db2cb44d91692500553f7e08e1e0501867a6ee6
Kanika Mishra <kanikamishra@Kanikas-MacBook-Pro.local>
2022-05-27 19:41:07 +0530

    minor

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from app.models import TwitterUsernameNftMap
 
-admin.site.register(TwitterUsernameNftMap)
+class TwitterUsernameNftMapAdmin(admin.ModelAdmin):
+    list_display = ('twitter_username','token_id','is_following')
+
+admin.site.register(TwitterUsernameNftMap,TwitterUsernameNftMapAdmin)
