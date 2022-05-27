26b8a031e44494addd045501e8d2bfd705c2425f
Kanika Mishra <kanikamishra@Kanikas-MacBook-Pro.local>
2022-05-27 19:30:44 +0530

    minor

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from app.models import TwitterUsernameNftMap
 
-admin.site(TwitterUsernameNftMap)
+admin.site.register(TwitterUsernameNftMap)
